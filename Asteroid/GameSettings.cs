2597aea69ca4c0b16fb0a883896b3a02d3f07ab0
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-04-02 15:48:12 +0300

    first commit

---
+++ Asteroid/GameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asteroid
+{
+    public static class GameSettings
+    {
+        public const int Width = 40;
+        public const int Height = 40;
+        public const int FrameDelayMs = 50;
+        public const char PlayerSymbol = 'A';
+    }
+}
---
+++ Asteroid/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Asteroid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
