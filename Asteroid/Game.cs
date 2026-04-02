1bfaac3f56297b5fd7c48831568ce154776c4a5e
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-04-03 00:49:56 +0300

    added game class

--- Asteroid/Game.cs
+++ Asteroid/Game.cs
@@ -4,7 +4,110 @@ using System.Text;
 
 namespace Asteroid
 {
-    internal class Game
+    public class Game
     {
+        private readonly int _width;
+        private readonly int _height;
+        private bool _isRunning;
+        private readonly Player _player;
+
+        public Game()
+        {
+            _width = GameSettings.Width;
+            _height = GameSettings.Height;
+
+            int playerStartX = _width / 2;
+            int playerStartY = _height - 2;
+
+            _player = new Player(playerStartX, playerStartY, GameSettings.PlayerSymbol);
+        }
+
+        public void Run()
+        {
+            InitializeConsole();
+            _isRunning = true;
+
+            while(_isRunning)
+            {
+                HandleInput();
+                Update();
+                Render();
+
+                Thread.Sleep(GameSettings.FrameDelayMs);
+            }
+        }
+
+        private void InitializeConsole()
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+        }
+
+        private void HandleInput()
+        {
+            while(Console.KeyAvailable)
+            {
+                ConsoleKey key = Console.ReadKey(intercept: true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        _player.MoveLeft(minX: 1);
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        _player.MoveRight(maxX: 1);
+                        break;
+
+                    case ConsoleKey.Escape:
+                        _isRunning = false;
+                        break;
+                }
+            }
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void Render()
+        {
+            Console.SetCursorPosition(0, 0);
+
+            StringBuilder buffer = new StringBuilder();
+
+            for (int y = 0; y < _height; y++)
+            {
+                for (int x = 0; x < _width; x++)
+                {
+                    bool isBorder =
+                        y == 0 ||
+                        y == _height - 1 ||
+                        x == 0 ||
+                        x == _width - 1;
+
+                    if ( isBorder )
+                    {
+                        buffer.Append('#');
+                    }
+                    else if (x == _player.X && y == _player.Y)
+                    {
+                        buffer.Append(_player.Symbol);
+                    }
+                    else
+                    {
+                        buffer.Append(' ');
+                    }
+                }
+                buffer.AppendLine();
+            }
+            buffer.Append($"Use Left/Right arrows to move, ESC to quit");
+
+            Console.Write(buffer.ToString());
+        }
     }
 }
+
+
+
--- Asteroid/Player.cs
+++ Asteroid/Player.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace Asteroid
 {
-    internal class Player
+    public class Player
     {
+        public int X { get; private set; }
+        public int Y { get; }
+        public char Symbol { get; }
+
+        public Player(int startX, int startY, char symbol)
+        {
+            X = startX;
+            Y = startY;
+            Symbol = symbol;
+        }
+
+        public void MoveLeft(int minX)
+        {
+            if (X > minX)
+            {
+                X--;
+            }
+        }
+        public void MoveRight(int maxX)
+        {
+            if (X < maxX)
+            {
+                X++;
+            }
+        }
     }
 }
